79c3e66f251880086dbfdbc46d29418b98bee359
moli <molix2@outlook.com>
2020-03-03 01:11:30 +0800

    add: 仓库页面调整

--- src/pages/namespace/repository/show.tsx
+++ src/pages/namespace/repository/show.tsx
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
-import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
-import { Menu, ClickParam } from 'antd';
+import {
+  SettingOutlined,
+  CodeOutlined,
+  IssuesCloseOutlined,
+  CloudDownloadOutlined,
+} from '@ant-design/icons';
+import {Menu, Popover} from 'antd';
 
 export default function(props: any) {
   const [current, setCurrent] = useState('code');
   const { SubMenu } = Menu;
 
-  const handleClick = function(e: ClickParam) {
-    console.log('click ', e);
+  const handleClick = (e: any) => {
+    console.log(e.key);
+    if (e.key == "clone") {
+      return;
+    }
     setCurrent(e.key);
   };
 
@@ -15,34 +23,31 @@ export default function(props: any) {
     <div>
       <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
         <Menu.Item key="code">
-          <MailOutlined />
+          <CodeOutlined />
           Code
         </Menu.Item>
-        <Menu.Item key="issue">
-          <AppstoreOutlined />
-          Issue
+        <Menu.Item key="issues">
+          <IssuesCloseOutlined />
+          Issues
         </Menu.Item>
-        <SubMenu
-          title={
-            <span className="submenu-title-wrapper">
-              <SettingOutlined />
-              Navigation Three - Submenu
-            </span>
-          }
-        >
-          <Menu.ItemGroup title="Item 1">
-            <Menu.Item key="setting:1">Option 1</Menu.Item>
-            <Menu.Item key="setting:2">Option 2</Menu.Item>
-          </Menu.ItemGroup>
-          <Menu.ItemGroup title="Item 2">
-            <Menu.Item key="setting:3">Option 3</Menu.Item>
-            <Menu.Item key="setting:4">Option 4</Menu.Item>
-          </Menu.ItemGroup>
-        </SubMenu>
-        <Menu.Item key="alipay">
-          <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-            Navigation Four - Link
-          </a>
+        <Menu.Item key="settings" style={{ float: 'right' }}>
+          <SettingOutlined />
+          Settings
+        </Menu.Item>
+        <Menu.Item key="clone" style={{ float: 'right' }} onBlur={()=>{}}>
+          <CloudDownloadOutlined />
+          <Popover
+            placement="bottomRight"
+            title="Clone or download"
+            content={
+              <div>
+                <div>http</div>
+                <div>ssh</div>
+              </div>
+            }
+          >
+            Clone or download
+          </Popover>
         </Menu.Item>
       </Menu>
     </div>
